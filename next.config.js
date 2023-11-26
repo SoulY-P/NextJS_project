30f60d58eb77eeda5a4e5025df9bf2adf6890746
SoulY P <128544105+SoulY-P@users.noreply.github.com>
2023-11-27 00:13:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    distDir: 'dist',
     images: {
       loader: 'custom',
       loaderFile: './my-loader.ts',
